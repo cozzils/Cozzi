c7b9ac548359972b1c6452017e4bd300492e81de
Luca Cozzi <cozziluca1@gmail.com>
2025-07-11 22:03:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/NOME-DEL-TUO-REPOSITORY/' // ← Questo deve corrispondere al nome del repo
+  base: '/Cozzi/' // ← Cambia qui! Il tuo repo si chiama "Cozzi"
 })
